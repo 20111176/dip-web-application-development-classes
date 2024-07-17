69285dffe19e7e15d6f98b8463aab216f369852f
Sukhwan Ko <20111176@tafe.wa.edu.au>
2024-07-18 01:30:18 +0800

    feat: chllenge 1 and 2 updated

--- session-01/assets/js_starter/scripts_challenge_1.js
+++ session-01/assets/js_starter/scripts_challenge_1.js
@@ -3,17 +3,14 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // // Define the function doubleNumbers
-// function doubleNumbers(arr) {
-//     let doubled = arr.map((number) => number * 2);
+function doubleNumbers(arr) {
+    let doubled = arr.map((number) => number * 2);
 
-//     return doubled;
-// }
-
-let doubleNumbers = (arr) => {arr.map((number) => number * 2)}
-
+    return doubled;
+}
 
 // Call the function with the numbers array
-let result =  doubleNumbers(numbers);
+let result = doubleNumbers(numbers);
 
 // Console log the new array
 console.log(result);
