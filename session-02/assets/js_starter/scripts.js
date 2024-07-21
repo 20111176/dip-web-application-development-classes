28a8f3464b95b71c7c94032b56b3bd0970ce504e
Cedric Ko <yacmov@gmail.com>
2024-07-21 23:28:19 +0800

    feat: challenge 1 updated

--- session-02/assets/js_starter/scripts.js
+++ session-02/assets/js_starter/scripts.js
@@ -1,1 +1,22 @@
+const fruits = ["banana", "pear", "apple"];
+// fruits.push("mango");
+// fruits.unshift("orange");
 
+fruits.push("mango, orange");
+fruits.unshift("mango, orange");
+console.log(...fruits);
+
+fruits.pop(); //  back of mango and orange
+fruits.shift(); // front of mango and orange
+fruits.splice(1, 1); // starting index, how many to remove
+
+console.log(...fruits);
+
+const items = ["a", "b", "c", "d", "e", "f"];
+console.log(...items);
+const i = 3;
+const filteredItems = items
+  .slice(0, i - 1)
+  .concat(items.slice(i, items.length));
+
+console.log(...filteredItems);
--- session-02/assets/js_starter/scripts_challenge_1.js
+++ session-02/assets/js_starter/scripts_challenge_1.js
@@ -1,11 +1,21 @@
 // Challenge 1 Starter
 // Initialise the array
 let numbers = [1, 2, 3, 4, 5];
+console.log(...numbers);
 
 // Use the pop() method to remove the last element from the array
+numbers.pop()
+console.log(...numbers);
 
 // Use the unshift() method to add the last element to the beginning of the array
+numbers.unshift(numbers.length)
+console.log(...numbers);
 
 // Use the slice() method to exclude the first 3 elements and return a new array
+let newNumbersArray = numbers.slice(3, numbers.length)
+console.log(...newNumbersArray);
 
 // Set the value of the outputfield inner html to the new array
+let outputField = document.getElementById('OutputField'); // select the output field
+outputField.innerHTML = newNumbersArray; // set its content to the new array
+console.log(...numbers);
