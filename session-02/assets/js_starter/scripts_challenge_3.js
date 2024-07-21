5eba87cd0ff7fbecd2a475cfcd457467bf4017ec
Cedric Ko <yacmov@gmail.com>
2024-07-22 01:20:18 +0800

    feat: session02 - challenge 3

--- session-02/assets/js_starter/scripts_challenge_3.js
+++ session-02/assets/js_starter/scripts_challenge_3.js
@@ -3,12 +3,23 @@
 let numbers = [1, 2, 3, 4, 5, 6];
 
 // Check if the array includes the number 5
+console.log(numbers)
+console.log(numbers.includes(5));
 
 // If true, filter out numbers less than 5
+const filteredB = numbers.filter((item) => item < 5);
+console.log(filteredB)
 
 // Then square each number in the resulting array
+const squiredArray = filteredB.map((item) => item * item);
 
 // Output the final array
+console.log(squiredArray)
+// squiredArray.push(5)
 
 // Output a message if the number 5 is not in the array
+if (!squiredArray.includes(5)) {
 console.log("The number 5 is not in the array.");
+} else {
+    console.log(`The number 5 is in the array.`);
+}
