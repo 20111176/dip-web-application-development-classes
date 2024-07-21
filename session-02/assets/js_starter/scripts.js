1ab91aeffc2e45fb78a7d6fb42afb0a71c6a4660
Cedric Ko <yacmov@gmail.com>
2024-07-22 00:27:48 +0800

    feat: session2 - Challendge 2 updated

--- session-02/assets/js_starter/scripts.js
+++ session-02/assets/js_starter/scripts.js
@@ -20,3 +20,31 @@ const filteredItems = items
   .concat(items.slice(i, items.length));
 
 console.log(...filteredItems);
+
+
+
+const list1 = [1, 2];
+const [first1, second1] = list1;
+console.log(first1);
+console.log(second1);
+
+
+
+const list2 = [1, 2, 3, 4, 5];
+const [first2, second2] = list2;
+console.log(first2);
+console.log(second2);
+
+const list3 = [1, 2, 3, 4, 5];
+const [first3, second3, third3, fourth3, fifth3] = list3;
+console.log(first3);
+console.log(second3);
+console.log(third3);
+console.log(fourth3);
+console.log(fifth3);
+
+const list4 = [1, 2, 3, 4, 5];
+const [first4, second4, ...others] = list4;
+console.log(first4);
+console.log(second4);
+console.log(others);
--- session-02/assets/js_starter/scripts_challenge_2.js
+++ session-02/assets/js_starter/scripts_challenge_2.js
@@ -4,9 +4,17 @@ let array1 = [1, 2, 3, 2];
 let array2 = [4, 5, 6, 4];
 
 // Use the spread operator to merge the two arrays
+let newArray = [...array1,...array2];
+console.log(newArray);
 
 // Use the Set object to remove duplicates
+let mySet = new Set(newArray);
+console.log(mySet);
 
 // Use array destructuring to assign the first three elements to variables
+let [first, second, third] = mySet;
+console.log(`${first}, ${second}, ${third}`)
 
 // Set the value of the output filed to be the second element of the unique array
+let outputFiled = document.getElementById('OutputField');
+outputFiled.innerText = second
