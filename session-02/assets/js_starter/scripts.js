502c9146ae67d9df9c9f82a6196fe6b4b062373c
Cedric Ko <yacmov@gmail.com>
2024-07-22 10:43:48 +0800

    feat: session2 Challenge 4 updated

--- session-02/assets/js_starter/scripts.js
+++ session-02/assets/js_starter/scripts.js
@@ -1,50 +1,55 @@
-const fruits = ["banana", "pear", "apple"];
-// fruits.push("mango");
-// fruits.unshift("orange");
+// const fruits = ["banana", "pear", "apple"];
+// // fruits.push("mango");
+// // fruits.unshift("orange");
 
-fruits.push("mango, orange");
-fruits.unshift("mango, orange");
-console.log(...fruits);
+// fruits.push("mango, orange");
+// fruits.unshift("mango, orange");
+// console.log(...fruits);
 
-fruits.pop(); //  back of mango and orange
-fruits.shift(); // front of mango and orange
-fruits.splice(1, 1); // starting index, how many to remove
+// fruits.pop(); //  back of mango and orange
+// fruits.shift(); // front of mango and orange
+// fruits.splice(1, 1); // starting index, how many to remove
 
-console.log(...fruits);
+// console.log(...fruits);
 
-const items = ["a", "b", "c", "d", "e", "f"];
-console.log(...items);
-const i = 3;
-const filteredItems = items
-  .slice(0, i - 1)
-  .concat(items.slice(i, items.length));
+// const items = ["a", "b", "c", "d", "e", "f"];
+// console.log(...items);
+// const i = 3;
+// const filteredItems = items
+//   .slice(0, i - 1)
+//   .concat(items.slice(i, items.length));
 
-console.log(...filteredItems);
-
-
-
-const list1 = [1, 2];
-const [first1, second1] = list1;
-console.log(first1);
-console.log(second1);
+// console.log(...filteredItems);
 
 
 
-const list2 = [1, 2, 3, 4, 5];
-const [first2, second2] = list2;
-console.log(first2);
-console.log(second2);
+// const list1 = [1, 2];
+// const [first1, second1] = list1;
+// console.log(first1);
+// console.log(second1);
 
-const list3 = [1, 2, 3, 4, 5];
-const [first3, second3, third3, fourth3, fifth3] = list3;
-console.log(first3);
-console.log(second3);
-console.log(third3);
-console.log(fourth3);
-console.log(fifth3);
 
-const list4 = [1, 2, 3, 4, 5];
-const [first4, second4, ...others] = list4;
-console.log(first4);
-console.log(second4);
-console.log(others);
+
+// const list2 = [1, 2, 3, 4, 5];
+// const [first2, second2] = list2;
+// console.log(first2);
+// console.log(second2);
+
+// const list3 = [1, 2, 3, 4, 5];
+// const [first3, second3, third3, fourth3, fifth3] = list3;
+// console.log(first3);
+// console.log(second3);
+// console.log(third3);
+// console.log(fourth3);
+// console.log(fifth3);
+
+// const list4 = [1, 2, 3, 4, 5];
+// const [first4, second4, ...others] = list4;
+// console.log(first4);
+// console.log(second4);
+// console.log(others);
+
+const a = [1, 4, 3, 2, 5];
+console.log(...a);
+a.sort((a, b) => (a > b ? 1 : -1));
+console.log(...a);
--- session-02/assets/js_starter/scripts_challenge_4.js
+++ session-02/assets/js_starter/scripts_challenge_4.js
@@ -1,13 +1,20 @@
 // Challenge 4 Starter
 // Initialise the array
 let numbers = [1, 2, 3, 4, 5, 6];
-
+let targetNumber = 5;
 // Find the index of the number 5 in the array
+let hasNumberFive = numbers.findIndex(x => x === targetNumber);
 
 
 // If the number 5 is found in the array
-
+if (hasNumberFive !== -1){
   // Console log each number in the array up to and including 5
-
+  numbers.forEach((element) => {
+    if (element > targetNumber) return;
+    console.log(element);
+  });
+}
+else {
   // Output a message if the number 5 is not in the array
   console.log("Number not in array")
+}
