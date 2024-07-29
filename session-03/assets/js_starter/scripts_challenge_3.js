d39cfe0d99ce2a264800f9a1901ecc0b11a7d7bf
Cedric Ko <yacmov@gmail.com>
2024-07-29 12:35:02 +0800

    feat: session 03 - challenge 3 updated

--- session-03/assets/js_starter/scripts_challenge_3.js
+++ session-03/assets/js_starter/scripts_challenge_3.js
@@ -1,10 +1,17 @@
 // Challenge 3 Starter
 // Create the class and use the constructor method
 class UserObject {
-
+    constructor(title = "None", year = 1900, author = "None"){
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
 }
 
 // Create an instance of the ComplexObject class (hint: add the properties)
-const userObjectInstance = {}
+const userObjectInstance1 = new UserObject();
+const userObjectInstance2 = new UserObject('The Great Gatsby', '1925', 'F. Scott Fitzgerald!');
 
 // Log the new instance of the object
+console.log(userObjectInstance1);
+console.log(userObjectInstance2);
